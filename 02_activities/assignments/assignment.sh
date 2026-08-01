0c43e04a2a070e1c484ed78bc00e60ca7a953fe1
ynahdy11 <ynahdy11@gmail.com>
2026-08-01 11:19:39 -0400

    assignment_completev3

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,59 +28,27 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
- mkdir data
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
- mv "C:\Users\Ynahd\documents\dsi\repos\shell\02_activities\assignments\rawdata" "C:\Users\Ynahd\documents\dsi\repos\shell\data"
- cd data
- mv mv rawdata raw
+mv 'C:\Users\Ynahd\Documents\DSI\shell\newproject\rawdata' 'C:\Users\Ynahd\Documents\DSI\shell\data'
+mv data/rawdata data/raw
 # 3. List the contents of the ./data/raw directory
- cd data
- cd raw
- ls raw
- event_log_26.log  ipaddr_8.txt       server_log_26.log  user_ipaddr_17.log  user_log_35.log
- event_log_27.log  ipaddr_9.txt       server_log_27.log  user_ipaddr_18.log  user_log_36.log
- event_log_28.log  misc_data.txt      server_log_28.log  user_ipaddr_19.log  user_log_37.log
- event_log_29.log  other_file_1.dat   server_log_29.log  user_ipaddr_2.log   user_log_38.log
- event_log_3.log   other_file_10.dat  server_log_3.log   user_ipaddr_20.log  user_log_39.log
- event_log_30.log  other_file_11.dat  server_log_30.log  user_ipaddr_3.log   user_log_4.log
- event_log_31.log  other_file_12.dat  server_log_31.log  user_ipaddr_4.log   user_log_40.log
- event_log_32.log  other_file_13.dat  server_log_32.log  user_ipaddr_5.log   user_log_41.log
- event_log_33.log  other_file_14.dat  server_log_33.log  user_ipaddr_6.log   user_log_42.log
- event_log_34.log  other_file_15.dat  server_log_34.log  user_ipaddr_7.log   user_log_43.log
- event_log_35.log  other_file_16.dat  server_log_35.log  user_ipaddr_8.log   user_log_44.log
- event_log_36.log  other_file_17.dat  server_log_36.log  user_ipaddr_9.log   user_log_45.log
- event_log_37.log  other_file_18.dat  server_log_37.log  user_log_1.log      user_log_46.log
- event_log_38.log  other_file_19.dat  server_log_38.log  user_log_10.log     user_log_47.log
- event_log_39.log  other_file_2.dat   server_log_39.log  user_log_11.log     user_log_48.log
- event_log_4.log   other_file_3.dat   server_log_4.log   user_log_12.log     user_log_49.log
- event_log_40.log  other_file_4.dat   server_log_40.log  user_log_13.log     user_log_5.log
- event_log_41.log  other_file_5.dat   server_log_41.log  user_log_14.log     user_log_50.log
- event_log_42.log  other_file_6.dat   server_log_42.log  user_log_15.log     user_log_6.log
- event_log_43.log  other_file_7.dat   server_log_43.log  user_log_16.log     user_log_7.log
- event_log_44.log  other_file_8.dat   server_log_44.log  user_log_17.log     user_log_8.log
- event_log_45.log  other_file_9.dat   server_log_45.log  user_log_18.log     user_log_9.log
+ls ./data/raw
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
- cd ..
- mkdir processed
- cd processed
- mkdir "server logs" "user logs" "event logs"
+mkdir -p ./processed/server_logs ./processed/user_logs ./processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
- cd ..
- cd raw
- cp *server* "C:\Users\Ynahd\documents\dsi\repos\shell\data\processed\server logs"
+cp ./raw/*server*.log ./processed/server_logs
 # 6. Repeat the above step for user logs and event logs
- cp *user* "C:\Users\Ynahd\documents\dsi\repos\shell\data\processed\user logs"
- cp *event* "C:\Users\Ynahd\documents\dsi\repos\shell\data\processed\event logs"
+cp ./raw/*user*.log ./processed/user_logs
+cp ./raw/*event*.log ./processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
- rm *ipaddr*
- cd "C:\Users\Ynahd\documents\dsi\repos\shell\data\processed\user logs"
-
+rm ./raw/*ipaddr* ./processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
- cd ../../
- mkdir inventory.txt
- cp -r "C:\Users\Ynahd\documents\dsi\repos\shell\data\processed" "C:\Users\Ynahd\documents\dsi\repos\shell\da
- ta\inventory.txt"
+touch ./data/inventory.txt
+echo ./processed/server_logs/*server_log* ./processed/user_logs/*user_log* ./processed/event_logs/*event_log* > inventory.txt 
+
+
 ###########################################
 
 echo "Project setup is complete!"
